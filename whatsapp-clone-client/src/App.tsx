d01186ee55412a5caf7a696250de9b47df97bad9
Braedon Gough <braedon@pleo.io>
2019-07-20 14:07:03 +0200

    Add chatslist screen with mock db

---
+++ whatsapp-clone-client/src/App.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+import ChatsListScreen from './components/ChatsListScreen';
+
+const App: FC = () => <ChatsListScreen />;
+
+export default App;
---
+++ whatsapp-clone-client/src/components/ChatsListScreen/ChatsNavbar.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ChatsNavbar = () => {
+  return <div>Whatsapp Clone</div>;
+};
+
+export default ChatsNavbar;
